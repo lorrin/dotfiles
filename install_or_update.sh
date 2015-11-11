ef9824c1ff9c0ea517e19355f16b2fa9168b9385
Lorrin Nelson <lhn@mult.ifario.us>
2015-11-11 15:57:38 -0800

    Update install_or_update.sh
    Allow local_* files to override

--- install_or_update.sh
+++ install_or_update.sh
@@ -41,7 +41,7 @@ stow --target=$HOME --dir="$DOTFILES_DIR" stows
 
 if [ -d "$DOTFILES_DIR/local_$(hostname -s)" ]; then
 	echo "Using local overrides ($DOTFILES_DIR/local_$(hostname -s))"
-	stow --target=$HOME --dir="$DOTFILES_DIR" "local_$(hostname -s)"
+	stow --override=".*" --target=$HOME --dir="$DOTFILES_DIR" "local_$(hostname -s)"
 else
 	echo "No local overrides found ($DOTFILES_DIR/local_$(hostname -s))"
 fi
